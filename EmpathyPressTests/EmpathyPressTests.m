bd305822abba59f12437352d21c383981b14fc4b
Nurtai <kcoder.2010@gmail.com>
2014-04-05 17:19:39 +0800

    init project

---
+++ EmpathyPressTests/EmpathyPressTests.m
@@ -0,0 +1,34 @@
+//
+//  EmpathyPressTests.m
+//  EmpathyPressTests
+//
+//  Created by Nurtai on 3/29/2557 BE.
+//  Copyright (c) 2557 BE Nurtai. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface EmpathyPressTests : XCTestCase
+
+@end
+
+@implementation EmpathyPressTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
